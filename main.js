0183ad58522b6042e6de3749efa52f28cd39321e
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-13 14:31:52 +0000

    ticket 3

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-alert("Hello World!");
-let songs = ["Snuff by Slipknot", "Stricken by Disturbed", "Creep by Radiohead", "No time to die by Billie Eilish", "Falling down by Lil Peep"];
-songs.length 
+let songs = [
+  "Snuff by Slipknot",
+  "Stricken by Disturbed",
+  "Creep by Radiohead",
+  "No time to die by Billie Eilish",
+  "Falling down by Lil Peep",
+];
+
+console.log(songs[1]);
+console.log(songs[0]);
+
+songs[4] = "Baby shark";
