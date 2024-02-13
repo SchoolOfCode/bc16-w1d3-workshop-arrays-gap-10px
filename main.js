8472ed4b193d53baade400a1b7c8fd6532cd75eb
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-13 15:24:49 +0000

    fin

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-let songs = [
-  "Snuff by Slipknot",
-  "Stricken by Disturbed",
-  "Creep by Radiohead",
-  "No time to die by Billie Eilish",
-  "Falling down by Lil Peep",
-];
+let songs = ["Snuff", "Stricken", "Creep", "No time to die", "Falling down"];
 
 console.log(songs[1]);
 console.log(songs[0]);
 
 songs[4] = "Baby shark";
+
+let cookiesOrdered = [15, 3, 67, 1, 22, 100];
+
+for (let i = 0; i < cookiesOrdered.length; i++) {
+  cookiesOrdered[i] *= 10;
+}
+
+console.log(cookiesOrdered);
