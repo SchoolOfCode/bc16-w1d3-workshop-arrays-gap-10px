9249a4e068a6c78173e7feb27cc0d5c3a9b9c90a
arseniyus <r.kozachenko2000@gmail.com>
2024-02-13 14:19:09 +0000

    ticket 2

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
 alert("Hello World!");
+let songs = ["Snuff by Slipknot", "Stricken by Disturbed", "Creep by Radiohead", "No time to die by Billie Eilish", "Falling down by Lil Peep"];
+songs.length 
